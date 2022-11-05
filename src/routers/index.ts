341b3370214fab166a4522a1967c2aba88bab581
Muhammad Ghifar <muhammadghifarshop@gmail.com>
2022-11-05 16:32:46 +0700

    fix: space controllers folder

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,11 @@
-import { Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-import { mangaChapters } from '../%20controllers/manga/chapters/index.ts';
-import { mangaDetails } from '../%20controllers/manga/details/index.ts';
+import { mangaChapters } from "../controllers/manga/chapters/index.ts";
+import { mangaDetails } from "../controllers/manga/details/index.ts";
 
 const router = new Router();
 
-router.post('/manga/chapters', mangaChapters);
-router.post('/manga/details', mangaDetails);
+router.post("/manga/chapters", mangaChapters);
+router.post("/manga/details", mangaDetails);
 
 export default router;
